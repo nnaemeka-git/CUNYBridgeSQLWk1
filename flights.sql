c30b006a3bab8f25d9ff73a6e7ce6be6ebe4988d
nnaemeka-git <64613292+nnaemeka-git@users.noreply.github.com>
2021-07-23 18:02:35 -0500

    Planes with different number of engines and the most number of seats?

--- flights.sql
+++ flights.sql
@@ -1,2 +1,4 @@
 
-SELECT dest, sum(distance) AS TOTAL_DISTANCE from flights GROUP BY dest order by TOTAL_DISTANCE desc;
+SELECT tailnum, sum(engines) AS Total_Engines, sum(seats) AS Total_Seats 
+from planes 
+GROUP BY tailnum order by Total_Engines DESC, Total_Seats DESC;
