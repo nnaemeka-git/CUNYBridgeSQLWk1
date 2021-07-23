e15d0e681e00ec2bc57a0e476b6d3acfb8344570
nnaemeka-git <64613292+nnaemeka-git@users.noreply.github.com>
2021-07-23 18:30:42 -0500

    The total number of flights.

--- flights.sql
+++ flights.sql
@@ -1,4 +1,1 @@
-
-SELECT tailnum, sum(engines) AS Total_Engines, sum(seats) AS Total_Seats 
-from planes 
-GROUP BY tailnum order by Total_Engines DESC, Total_Seats DESC;
+SELECT distinct count(*) as "Total Flights" from flights;
